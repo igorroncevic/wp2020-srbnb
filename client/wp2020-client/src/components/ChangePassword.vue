84a8c2d91067d02fdd5e3d3fd0f9c16dc52fddf6
igorroncevic <ironcevic98@gmail.com>
2020-05-20 20:59:48 +0200

    FE - Change Password

--- client/wp2020-client/src/components/ChangePassword.vue
+++ client/wp2020-client/src/components/ChangePassword.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="">
-     Change Password
+     <form action method="post">
+      <SimpleInput label="Current Password" v-model="currentPassword" :inputValue="''" />
+      <SimpleInput label="New Password" v-model="newPassword" :inputValue="''" />
+      <SimpleInput label="Confirm" v-model="confirmPassword" :inputValue="''" />
+      <Button text="Save Changes" width="300" fontsize="20" id="button" @clicked="submitForm" />
+    </form>
   </div>
 </template>
 
 <script>
-export default {
+import SimpleInput from "./form-components/SimpleInput.vue";
+import Button from "./form-components/Button.vue";
 
-}
+export default {
+  components: {
+    SimpleInput,
+    Button
+  },
+  data() {
+    return {
+      currentPassword: "",
+      newPassword: "",
+      confirmPassword: ""
+    };
+  },
+  methods: {
+    submitForm(){
+       console.log("Current Password: " + this.currentPassword + ", new password: " + this.newPassword + ", confirm: " + this.confirmPassword);
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+#button {
+  margin-left: 6rem;
+  margin-top: 6rem;
+}
 
 </style>
