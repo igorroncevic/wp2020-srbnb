ad1b2beacab04549ab01eb44e63b2faef2bd6f4f
zukovicfilip <zukovicfilip@gmail.com>
2020-06-25 11:56:49 +0200

    BE - Switched authentication to JWT

--- backend/SparkBackend/src/rest/SparkAppMain.java
+++ backend/SparkBackend/src/rest/SparkAppMain.java
@@ -8,6 +8,9 @@ import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 import java.io.File;
+import java.util.Date;
+import java.security.Key;
+import io.jsonwebtoken.security.Keys;
 
 import org.eclipse.jetty.security.UserAuthentication;
 
@@ -15,11 +18,14 @@ import com.google.gson.Gson;
 
 import beans.users.*;
 import dao.UsersDAO;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import spark.Session;
 
 public class SparkAppMain {
 
 	private static Gson g = new Gson();
+	static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
 	public static void main(String[] args) throws Exception {
 		
@@ -61,9 +67,7 @@ public class SparkAppMain {
 				res.status(400);
 				return "Failed to login";
 			} else {
-				Session ss = req.session(true);
-				ss.attribute("user", user);
-				return "Successful login";
+				return Jwts.builder().setSubject(user.getUsername()).setIssuedAt(new Date()).signWith(key).compact();
 			}
 			
 		});
